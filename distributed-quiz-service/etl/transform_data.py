8b3fe679b0869e4bfba8de7a03c2337214cbc4f7
unknown <harshana senadeera>
2025-04-03 11:31:57 +0530

    Add Update 33

--- distributed-quiz-service/etl/transform_data.py
+++ distributed-quiz-service/etl/transform_data.py
@@ -7,7 +7,7 @@ def transform_data():
     transformed_df = df.groupby("user_id")["score"].mean().reset_index()
     transformed_df.to_csv("transformed_data.csv", index=False)
 
-    print("Data transformed successfully...!")
+    print("Data transformed successfully....!")
 
 if __name__ == "__main__":
     transform_data()
