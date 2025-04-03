1427fea4a3241d0dc072eee4260ff2dfd92b2150
unknown <harshana senadeera>
2025-04-03 10:49:55 +0530

    Add Update 25

--- distributed-quiz-service/etl/load_data.py
+++ distributed-quiz-service/etl/load_data.py
@@ -8,7 +8,7 @@ def load_data():
     engine = sqlalchemy.create_engine(DW_URI)
     df = pd.read_csv("extracted_data.csv")
     df.to_sql("quiz_results_dw", engine, if_exists="replace", index=False)
-    print("Data loaded successfully!")
+    print("Data loaded successfully...!")
 
 if __name__ == "__main__":
     load_data()
